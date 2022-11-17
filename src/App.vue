2627848160ce54a031b8dfb64224817e4ae5ff30
grievous0 <jvopereira2004@gmail.com>
2022-11-17 09:51:57 -0300

    Add first component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <header>
-  </header>
-
-  <main>
-  </main>
+  <section id="App">
+    <main>
+      <HomePage />
+    </main>
+  </section>
 </template>
 
 <script type="text/javascript">
+"use strict";
+import HomePage from './components/HomePage.vue';
 
+export default {
+  "name": 'App',
+  "components": {
+    "HomePage": HomePage,
+  },
+  "data": function() {
+    return {}
+  },
+};
+  
 </script>
 <style scoped>
 
---
+++ src/components/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+        <h1>Home</h1>
+        <p>This is the home page.</p>
+    </div>
+</template>
+
+<script type="text/javascript">
+"use strict";
+
+export default {
+    "name": "HomePage",
+    "components": {
+    },
+    "data": function() {
+        return {
+        }
+    }
+}
+
+</script>
